d07b9adaf2e15688247fb0d3f4c8f245007a134a
Iskandar Muhaemin <iskandar.muhaemin@danasaham.co.id>
2023-12-18 11:57:15 +0700

    contoh kasus

--- lib/data.dart
+++ lib/data.dart
@@ -1,69 +1,40 @@
-final List<Map<String, dynamic>> edgesData = [
-  {
-    'source': 'UNPER',
-    'destination': 'Jalan Batara',
-    'lat': -7.3546681,
-    'long': 108.2245863
-  },
-  {
-    'source': 'Jalan Batara',
-    'destination': 'Jalan Letjen Mashudi',
-    'lat': -7.3535989,
-    'long': 108.2352727
-  },
-  {
-    'source': 'Jalan Letjen Mashudi',
-    'destination': 'Perumahan Pelangi',
-    'lat': -7.3594113,
-    'long': 108.2381173
-  },
-  {
-    'source': 'Jalan Siliwangi',
-    'destination': 'Cilolohan',
-    'lat': -7.347188,
-    'long': 108.2332179
-  },
-  {
-    'source': 'Cilolohan',
-    'destination': 'Jalan Letjen Mashudi',
-    'lat': -7.3492384,
-    'long': 108.2359775
-  },
-];
-
-void main() {
-  Map<String, Map<String, double>> adjacencyList = {
-    "UNPER": {"Jalan Batara": 0.2, "Jalan Letjen Mashudi": 1.5},
-    "Jalan Batara": {"Jalan Letjen Mashudi": 1.3, "Cilolohan": 2.5},
-    "Jalan Letjen Mashudi": {"Jalan Siliwangi": 1.2, "Cilolohan": 1.8},
-    "Cilolohan": {"Perumahan Pelangi Residence": 0.5},
-    "Jalan Siliwangi": {"Perumahan Pelangi Residence": 1.0},
-  };
-
-  // Update data for UNPER
-  adjacencyList["UNPER"] = {
+final Map<String, Map<String, double>> adjacencyList = {
+  "Unper": {
     "Jalan Batara": 0.35,
     "Sukanagara": 1.4,
-    "Jalan Letjen Mashudi": 1.0,
-    "Tujuan": 0.6,
-  };
+    "Jalan Letnan Mashudi": 1.65,
+    "Perumahan Pelangi Residence": 1
+  },
+  "Jalan Batara": {
+    "Unper": 0.35,
+    "Sukanagara": 1.1,
+    "Jalan Letnan Mashudi": 1,
+    "Perumahan Pelangi Residence": 0.6
+  },
+  "Sukanagara": {
+    "Unper": 1.4,
+    "Jalan Batara": 1.1,
+    "Jalan Letnan Mashudi": 0.27,
+    "Perumahan Pelangi Residence": 1.2
+  },
+  "Jalan Letnan Mashudi": {
+    "Unper": 1.65,
+    "Jalan Batara": 1,
+    "Sukanagara": 0.27,
+    "Perumahan Pelangi Residence": 0
+  },
+  "Perumahan Pelangi Residence": {
+    "Unper": 1,
+    "Jalan Batara": 0.6,
+    "Sukanagara": 1.2,
+    "Jalan Letnan Mashudi": 0
+  }
+};
 
-  // Update data for Jalan Batara
-  adjacencyList["Jalan Batara"] = {
-    "Sukanagara": 1.4,
-    "Jalan Letjen Mashudi": 1.0,
-    "Tujuan": 0.6,
-  };
-
-  // Update data for Jalan Letjen Mashudi
-  adjacencyList["Jalan Letjen Mashudi"] = {
-    "Siliwangi": 1.2,
-    "Cilolohan": 0.27,
-    "Tujuan": 1.2,
-  };
-
-  // Print the updated adjacencyList
-  adjacencyList.forEach((key, value) {
-    print('$key: $value');
-  });
-}
+Map<String, Map<String, double>> adjacencyList2 = {
+  "A": {"B": 1, "C": 3, "D": 7},
+  "B": {"A": 1, "C": 1, "E": 5},
+  "C": {"A": 3, "B": 1, "D": 2, "E": 1},
+  "D": {"A": 7, "C": 2, "E": 2},
+  "E": {"B": 5, "C": 1, "D": 2},
+};
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:dijkstra_app/astar.dart';
 import 'package:dijkstra_app/data.dart';
-import 'package:dijkstra_app/dijkstra.dart';
-import 'package:dijkstra_app/dijkstra2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,124 +15,29 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var data = [];
-
-  void dijkstra() {
-    // Membuat objek graf
-    Graph graph = Graph();
-
-    // Menambahkan edge berdasarkan studi kasus
-    for (var element in edgesData) {
-      graph.addEdge(element['source'], element['destination']);
-    }
-
-    try {
-      // Menjalankan algoritma Dijkstra
-      List<Map<String, Object>> results =
-          graph.dijkstra('UNPER', 'Perumahan Pelangi');
-
-      String option = '';
-
-      // Menampilkan hasil
-      for (int i = 0; i < results.length; i++) {
-        print('Opsi Rute ${i + 1}: ${results[i]['shortestPath']}');
-        print('Jarak Terpendek: ${results[i]['distance']} km\n');
-        setState(() {
-          option =
-              'Rute : ${results[i]['shortestPath']} dengan jarak : ${results[i]['distance']} km\n';
-
-          data.add(option);
-        });
-      }
-
-      print(data);
-    } on Exception catch (e) {
-      print(e);
-    }
-  }
+  String option = '';
 
   void aStar() {
-    // Membuat objek graf
-    Graph graph = Graph();
-
-    // Menambahkan edge berdasarkan studi kasus
-    for (var element in edgesData) {
-      graph.addEdge(element['source'], element['destination']);
-    }
-
-    try {
-      // Menjalankan algoritma A*
-      List<Map<String, Object>> results =
-          graph.aStar('UNPER', 'Perumahan Pelangi');
-
-      String option = '';
-
-      // Menampilkan hasil
-      for (int i = 0; i < results.length; i++) {
-        print('Opsi Rute ${i + 1}: ${results[i]['shortestPath']}');
-        print('Jarak Terpendek: ${results[i]['distance']} km\n');
-        setState(() {
-          option =
-              'Rute : ${results[i]['shortestPath']} dengan jarak : ${results[i]['distance']} km\n';
-
-          data.add(option);
-        });
-      }
-
-      print(data);
-    } on Exception catch (e) {
-      print(e);
-    }
-  }
-
-  void main() {
     // Data graf
-    Map<String, Map<String, double>> adjacencyList = {
-      "A": {"B": 1, "C": 3, "D": 7},
-      "B": {"A": 1, "C": 1, "E": 5},
-      "C": {"A": 3, "B": 1, "D": 2, "E": 1},
-      "D": {"A": 7, "C": 2, "E": 2},
-      "E": {"B": 5, "C": 1, "D": 2},
-    };
-
-    // Objek graf
-    Graph2 graph2 = Graph2(adjacencyList: adjacencyList);
-
-    // Cari rute dari Node A ke Node E
-    List<Map<String, Object>> results = graph2.dijkstra("A", "E");
-
-    // Tampilkan hasil
-    for (Map<String, Object> result in results) {
-      print("**Rute ${result['distance']} km**");
-      print(result['shortestPath']);
-    }
-  }
-
-  void main2() {
-    // Data graf
-    Map<String, Map<String, double>> adjacencyList = {
-      "UNPER": {
-        "Jalan Batara": 0.2,
-        "Jalan Letjen Mashudi": 1.5,
-        "Sukaraja": 1.4,
-        "Jalan Siliwangi": 0.45
-      },
-      "Jalan Batara": {"Jalan Letjen Mashudi": 1.3, "Cilolohan": 2.5},
-      "Jalan Letjen Mashudi": {"Jalan Siliwangi": 1.2, "Cilolohan": 1.8},
-      "Cilolohan": {"Perumahan Pelangi Residence": 0.5},
-      "Jalan Siliwangi": {"Perumahan Pelangi Residence": 1.0}
-    };
 
     // Objek graf
     Graph3 graph3 = Graph3(adjacencyList: adjacencyList);
 
     // Cari rute dari A ke E dengan minimal 2 atau 3 opsi
     List<Map<String, Object>> results =
-        graph3.aStar("UNPER", "Perumahan Pelangi Residence", 3);
+        graph3.aStar("Unper", "Perumahan Pelangi Residence", 2);
 
     // Tampilkan hasil
     for (int i = 0; i < results.length; i++) {
       print("**Rute ${results[i]['distance']} km (Opsi ${i + 1})**");
       print(results[i]['shortestPath']);
+
+      setState(() {
+        option =
+            'Rute : ${results[i]['shortestPath']} dengan jarak : ${results[i]['distance']} km\n';
+
+        data.add(option);
+      });
     }
   }
 
@@ -167,7 +70,7 @@ class _MyAppState extends State<MyApp> {
               .toList(),
         )),
         floatingActionButton: FloatingActionButton(
-          onPressed: main2,
+          onPressed: aStar,
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ), // This trailing comma makes
